377a0889a07e12bee80f86f0eb2afebb30fafb64
aneeshseth <aneeshseth2018@gmail.com>
2024-01-15 15:07:16 +0400

    next

---
+++ .history/apps/fe/src/app/chats/page_20240115150232.tsx
@@ -0,0 +1,23 @@
+
+import Image from "next/image";
+
+import { Mail } from "@/app/chats/components/main";
+import { accounts } from "@/app/chats/data";
+import { redirect } from "next/navigation";
+
+
+export default function MailPage() {
+
+ // const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  //const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+  return (
+    <>
+      <Mail
+        accounts={accounts}
+      // defaultLayout={defaultLayout}
+       // defaultCollapsed={defaultCollapsed}
+        navCollapsedSize={5}
+      />
+    </>
+  );
+}
--- .history/apps/fe/src/app/chats/page_20240115150255.tsx
+++ .history/apps/fe/src/app/chats/page_20240115150255.tsx
@@ -1,22 +1,20 @@
-import { cookies } from "next/headers";
 
+import Image from "next/image";
 
 import { Mail } from "@/app/chats/components/main";
 import { accounts } from "@/app/chats/data";
-
+import { redirect } from "next/navigation";
 
 
 export default function MailPage() {
-  //const layout = cookies().get("react-resizable-panels:layout");
-//  const collapsed = cookies().get("react-resizable-panels:collapsed");
 
-  //const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+ // const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
   //const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
   return (
     <>
       <Mail
         accounts={accounts}
-        //defaultLayout={defaultLayout}
+        // defaultLayout={defaultLayout}
         // defaultCollapsed={defaultCollapsed}
         navCollapsedSize={5} defaultLayout={undefined}      />
     </>
