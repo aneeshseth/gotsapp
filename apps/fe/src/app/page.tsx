33060d4fcc5f3cdc71b0caa792aad26cc7038800
Aneesh Seth <122401851+aneeshseth@users.noreply.github.com>
2024-01-26 01:18:36 -0800

    fe bug

--- apps/fe/src/app/page.tsx
+++ apps/fe/src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           <Button
             className="bg-transparent  text-white hover:bg-white hover:text-black"
             onClick={() => {
-              router.push("/how");
+              router.push("/auth");
             }}
           >
             Start Chatting
